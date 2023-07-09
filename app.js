e7626ce1d01b24d875e326aaa762d595fdfc9d78
Jonathan Villa <78127302+jothaku@users.noreply.github.com>
2023-07-08 20:09:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
+const listViewRouter = require("./list-view-router.js");
+const listEditRouter = require("./list-edit-router.js");
 
-const tasks = [
-  {
-    id: "123456",
-    isCompleted: false,
-    description: "Walk the dog",
-  },
-];
+app.use(express.json());
+
+let tasks = [];
+let taskIdCounter = 1;
+
+// Agregar los routers a las rutas principales
+app.use("/edit", listEditRouter(tasks, taskIdCounter));
+app.use("/view", listViewRouter(tasks));
 
 app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log("Servidor en funcionamiento en el puerto 3000.");
 });
